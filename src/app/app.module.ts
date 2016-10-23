a05126d9fba97e0abce4a10b3a2938e9d2e10902
cdelmoral <carderomad@gmail.com>
2016-10-23 16:23:57 -0500

    Add home and help pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { HelpComponent } from './pages/help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'help', component: HelpComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
