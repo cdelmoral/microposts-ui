9a955df93782cef0ad1b348d0def1262deb0a8d6
Carlos del Moral <carderomad@gmail.com>
2016-11-29 00:05:28 -0600

    Create service method to get the microposts feed for a specific user

--- src/app/microposts/microposts.service.ts
+++ src/app/microposts/microposts.service.ts
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/toPromise';
 
 import { environment } from '../../environments/environment';
 
+import { Micropost } from './micropost';
 import { AuthService } from '../auth/auth.service';
 import { User } from '../users/user';
 
@@ -23,10 +24,35 @@ export class MicropostsService {
 
     try {
       let response = await this.http.post(url, body, this.options).toPromise();
+      // TODO: return User but do not setup the current user
       this.authService.currentUser = new User(response.json());
       return this.authService.currentUser;
     } catch(e) {
       throw e;
     }
+  }
+
+  public async getMicropostsFeedPage(user: User, page: number) {
+    let url = environment.serverUrl + '/microposts/feed/' + user.id + '?pageNumber=' +
+      page + '&itemsPerPage=' + 10;
+
+    try {
+      let response = await this.http.get(url, this.options).toPromise();
+      let responseJson = response.json();
+      let microposts = this.createMicropostsArray(responseJson.microposts, user);
+      return { count: responseJson.count, microposts: microposts };
+    } catch(e) {
+      throw e;
+    }
+  }
+
+  private createMicropostsArray(responseMicroposts: any, user: User): Array<Micropost> {
+    let microposts = new Array<Micropost>();
+    for (let micropost of responseMicroposts) {
+      micropost.user_name = user.name;
+      micropost.user_id = user.id;
+      microposts.push(new Micropost(micropost));
+    }
+    return microposts;
   }
 }
