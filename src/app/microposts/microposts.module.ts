ef02ce213d582c418107410c6b4a26f16ac35ac3
Carlos del Moral <carderomad@gmail.com>
2016-11-29 00:08:24 -0600

    Add new declarations, exports and imports to microposts module

--- src/app/microposts/microposts.module.ts
+++ src/app/microposts/microposts.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { PaginationModule } from '../pagination/pagination.module';
 import { MicropostsService } from './microposts.service';
 import { NewMicropostComponent } from './new-micropost/new-micropost.component';
+import { MicropostsFeedComponent } from './microposts-feed/microposts-feed.component';
+import { MicropostComponent } from './micropost/micropost.component';
 
 @NgModule({
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    PaginationModule
   ],
   exports: [
-    NewMicropostComponent
+    NewMicropostComponent,
+    MicropostsFeedComponent
   ],
-  declarations: [NewMicropostComponent],
+  declarations: [
+    NewMicropostComponent,
+    MicropostsFeedComponent,
+    MicropostComponent
+  ],
   providers: [
     MicropostsService
   ]
