948ce961eaa0b24a957e1d1f839612fdff49e195
Carlos del Moral <carderomad@gmail.com>
2016-12-17 13:39:32 -0600

    Refresh list of microposts in user page when deleting a micropost

--- src/app/microposts/micropost/micropost.component.ts
+++ src/app/microposts/micropost/micropost.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 import { Micropost } from '../micropost';
 import {AuthService} from '../../auth/auth.service';
@@ -14,11 +14,15 @@ export class MicropostComponent {
     @Input()
     public micropost: Micropost;
 
+    @Output()
+    public onDelete: EventEmitter<Micropost> = new EventEmitter();
+
     constructor(public authService: AuthService, private micropostsService: MicropostsService) {}
 
     public async deleteMicropost() {
         try {
             await this.micropostsService.deleteMicropost(this.micropost.userId, this.micropost.id);
+            this.onDelete.emit(this.micropost);
         } catch (e) {
             throw e;
         }
