845ef8de00779a0ff6a0951bae9a08a2371a4fdd
Carlos del Moral <carderomad@gmail.com>
2016-12-18 23:41:23 -0600

    Implement follow/unfollow functionality

--- src/app/users/user-follow-button/user-follow-button.component.ts
+++ src/app/users/user-follow-button/user-follow-button.component.ts
@@ -1,22 +1,45 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../user';
+import {AuthService} from '../../auth/auth.service';
+import {UsersService} from '../users.service';
 
 @Component({
     selector: 'mp-user-follow-button',
     templateUrl: './user-follow-button.component.html',
     styles: []
 })
-export class UserFollowButtonComponent {
+export class UserFollowButtonComponent implements OnInit {
 
     @Input()
     public user: User;
 
+    public followLabel: string;
+
     public isFollowing: boolean;
 
-    constructor() { this.isFollowing = false; }
+    constructor(private authService: AuthService, private usersService: UsersService) { }
+
+    async ngOnInit() {
+        this.isFollowing =
+            await this.usersService.isFollowing(this.authService.currentUser.id, this.user.id);
+        this.followLabel = this.isFollowing ? 'Unfollow' : 'Follow';
+    }
 
     public async onSubmit() {
-        this.isFollowing = !this.isFollowing;
+        try {
+            if (this.isFollowing) {
+                await this.usersService.unfollow(this.user.id);
+                this.user.followersCount--;
+            } else {
+                await this.usersService.follow(this.user.id);
+                this.user.followersCount++;
+            }
+
+            this.isFollowing = !this.isFollowing;
+            this.followLabel = this.isFollowing ? 'Unfollow' : 'Follow';
+        } catch (e) {
+            throw e;
+        }
     }
 
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { environment } from '../../environments/environment';
@@ -9,60 +9,91 @@ import { User } from '../users/user';
 
 @Injectable()
 export class UsersService {
-  private usersUrl: string = environment.serverUrl + '/users';
-  private options: RequestOptions = new RequestOptions({
-    headers: new Headers({ 'Content-Type' : 'application/json '}),
-    withCredentials: true
-  });
+    private usersUrl: string = environment.serverUrl + '/users';
+    private options: RequestOptions = new RequestOptions({
+        headers: new Headers({ 'Content-Type' : 'application/json '}),
+        withCredentials: true
+    });
 
-  constructor(private http: Http, private authService: AuthService) {}
+    constructor(private http: Http, private authService: AuthService) {}
 
-  public async getUser(userId: string) {
-    let url = this.usersUrl + '/' + userId;
+    public async getUser(userId: string) {
+        let url = this.usersUrl + '/' + userId;
 
-    try {
-      let response = await this.http.get(url, this.options).toPromise();
-      return new User(response.json());
-    } catch(e) {
-      throw e;
-    }
-  }
-
-  public async getPage(page: number, items: number) {
-    let url = this.usersUrl + '/index_page?pageNumber=' + page + '&usersPerPage=' + items;
-
-    try {
-      let response = await this.http.get(url, this.options).toPromise();
-      return response.json();
-    } catch(e) {
-      throw e;
-    }
-  }
-
-  public async createUser(user: any) {
-    let body = JSON.stringify(user);
-
-    try {
-      let response = await this.http.post(this.usersUrl, body, this.options).toPromise();
-      return response.json();
-    } catch(e) {
-      throw e;
-    }
-  }
-
-  public async activateUser(userId: string, token: string): Promise<boolean> {
-    let url = this.usersUrl +'/activate/' + userId + '/' + token;
-    let data;
-
-    try {
-      let response = await this.http.put(url, {}, this.options).toPromise();
-      console.log(response.json());
-      data = response.json();
-    } catch(e) {
-      throw e;
+        try {
+            let response = await this.http.get(url, this.options).toPromise();
+            return new User(response.json());
+        } catch (e) {
+            throw e;
+        }
     }
 
-    return data && data.user;
-  }
+    public async getPage(page: number, items: number) {
+        let url = this.usersUrl + '/index_page?pageNumber=' + page + '&usersPerPage=' + items;
+
+        try {
+            let response = await this.http.get(url, this.options).toPromise();
+            return response.json();
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    public async createUser(user: any) {
+        let body = JSON.stringify(user);
+
+        try {
+            let response = await this.http.post(this.usersUrl, body, this.options).toPromise();
+            return response.json();
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    public async activateUser(userId: string, token: string): Promise<boolean> {
+        let url = this.usersUrl + '/activate/' + userId + '/' + token;
+        let data;
+
+        try {
+            let response = await this.http.put(url, {}, this.options).toPromise();
+            data = response.json();
+        } catch (e) {
+            throw e;
+        }
+
+        return data && data.user;
+    }
+
+    public async isFollowing(followerId: string, followingId: string) {
+        let url = this.usersUrl + '/' + followerId + '/is_following/' + followingId;
+
+        try {
+            let response = await this.http.get(url, this.options).toPromise();
+            let data = response.json();
+            return data.is_following;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    public async follow(followingId: string) {
+        let url = this.usersUrl + '/' + followingId + '/follow';
+
+        try {
+            let response = await this.http.post(url, {}, this.options).toPromise();
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    public async unfollow(followingId: string) {
+        let url = this.usersUrl + '/' + followingId + '/unfollow';
+
+        try {
+            let response = await this.http.delete(url, this.options).toPromise();
+        } catch (e) {
+            throw e;
+        }
+    }
 
 }
