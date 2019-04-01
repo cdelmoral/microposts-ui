720e2fe0ead50c0092f24ece92f013a39b2b363c
Carlos del Moral <carderomad@gmail.com>
2019-03-31 22:52:11 -0500

    Add flash message for user activation and minor changes

--- src/app/users/user-activation/user-activation.component.ts
+++ src/app/users/user-activation/user-activation.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { FlashMessagesService } from 'angular2-flash-messages';
+
 @Component({
   selector: 'mp-user-activation',
   template: '',
@@ -10,11 +12,16 @@ export class UserActivationComponent implements OnInit {
 
   private activated: boolean;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private flashService: FlashMessagesService
+  ) { }
 
   ngOnInit() {
     this.route.data.subscribe((data: { activated: boolean }) => {
       if (data.activated) {
+        this.flashService.show('The account has been activated.', {timeout: 5000, cssClass: 'alert-success'});
         this.router.navigate(['/login']);
       } else {
         this.router.navigate(['/signup']);
